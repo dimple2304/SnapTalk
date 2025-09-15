1bb47ae8bbf66a9d676cc2a81600a379a31d0353
dimple2304 <dimplekumar2304@gmail.com>
2025-09-15 11:19:43 +0530

    auth pages

--- backend/routes/frontend.routes.js
+++ backend/routes/frontend.routes.js
@@ -6,9 +6,13 @@ router.get('/', (req, res) => {
     res.render("registration");
 })
 
-// router.get('/login', (req, res) => {
-//     res.render("login");
-// })
+router.get('/registration', (req, res) => {
+    res.render("auth/registration");
+})
+
+router.get('/login', (req, res) => {
+    res.render("auth/login");
+})
 
 router.get('/homepage', (req, res) => {
     res.render("homepage");
