83e6d0cb282370e47ad61521eb324eefc1d770d3
dimple2304 <dimplekumar2304@gmail.com>
2026-06-11 14:08:20 +0530

    create post modal implemented

--- frontend/js/createPost.js
+++ frontend/js/createPost.js
@@ -1,99 +1,134 @@
-const postMediaInput = document.querySelector("#post-media-input");
-const thought = document.querySelector("#thought");
-const previewDiv = document.querySelector("#preview-div");
-const postBtn = document.querySelector("#post-btn");
-const previewSpinner = document.querySelector("#preview-spinner #spinner");
-const createPostSpinner = document.querySelector("#create-post-spinner #spinner");
-const createPostError = document.querySelector("#create-post-error");
-const postdynamicimg = document.querySelector(".postdynamicimg");
+
+const createPostModal = document.querySelector("#create-post-modal");
+function createPostFunc(container = document) {
+    const postMediaInput = container.querySelector(".post-media-input");
+    const thought = container.querySelector(".thought");
+    const previewDiv = container.querySelector("#preview-div");
+    const postBtn = container.querySelector(".post-btn");
+    const previewSpinner = container.querySelector("#preview-spinner #spinner");
+    const createPostSpinner = container.querySelector("#create-post-spinner #spinner");
+    const createPostError = container.querySelector("#create-post-error");
+    const postdynamicimg = container.querySelector(".postdynamicimg");
 
 
-let fileId;
+    let fileId;
 
-const mediaPreview = document.createElement("img");
-previewDiv.appendChild(mediaPreview);
+    let mediaPreview = previewDiv.querySelector("img");
+    if (!mediaPreview) {
+        mediaPreview = document.createElement("img");
+        previewDiv.appendChild(mediaPreview);
+    }
 
-postMediaInput.addEventListener("change", async function (e) {
-    const mediaFiles = postMediaInput.files;
-    if (mediaFiles.length > 0) {
-        postBtn.disabled = false;
-        const file = e.target.files[0];
-        console.log(file);
-        previewSpinner.classList.remove("hidden");
+    postMediaInput.addEventListener("change", async function (e) {
+        const mediaFiles = postMediaInput.files;
+        if (mediaFiles.length > 0) {
+            const file = e.target.files[0];
+            console.log(file);
+            previewSpinner.classList.remove("hidden");
+            postBtn.disabled = false;
 
-        const formData = new FormData();
-        formData.append("file", file);
-        formData.append("fileName", file.name);
-        formData.append("folder", "postsMedia");
+            const formData = new FormData();
+            formData.append("file", file);
+            formData.append("fileName", file.name);
+            formData.append("folder", "postsMedia");
 
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = function (event) {
-                mediaPreview.classList.add("w-45", "max-h-[350px]", "h-45", "mt-4", "object-cover", "border-4", "border-slate-200");
-                mediaPreview.src = event.target.result;
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function (event) {
+                    mediaPreview.classList.add("w-45", "max-h-[350px]", "h-45", "mt-4", "object-cover", "border-4", "border-slate-200");
+                    mediaPreview.src = event.target.result;
+                }
+                reader.readAsDataURL(file);
             }
-            reader.readAsDataURL(file);
+
+            const res = await fetch("/api/account/upload-file", {
+                method: "POST",
+                body: formData
+            })
+            const data = await res.json();
+            if (!res.ok) {
+                previewSpinner.classList.add("hidden");
+                createPostError.innerHTML = data.message;
+                return;
+            }
+            mediaPreview.src = data.fileUrl
+            fileId = data.fileId
+
+            previewSpinner.classList.add("hidden");
+            console.log(data);
+
         }
-
-        const res = await fetch("/api/account/upload-file", {
-            method: "POST",
-            body: formData
-        })
-        const data = await res.json();
-        mediaPreview.src = data.fileUrl
-        fileId = data.fileId
-
-        previewSpinner.classList.add("hidden");
-        console.log(data);
-
-    }
-})
-
-thought.addEventListener("input", function () {
-    const thoughtValue = thought.value.trim();
-    if (thoughtValue) { postBtn.disabled = false }
-    else { postBtn.disabled = true };
-})
-
-postBtn.addEventListener("click", async function () {
-    const mediaVal = mediaPreview.getAttribute("src");
-    const thoughtVal = thought.value.trim();
-
-    createPostError.innerHTML = "";
-
-    createPostSpinner.classList.remove("hidden");
-
-
-    const content = {
-        url: mediaVal,
-        fileId: fileId,
-        thought: thoughtVal
-    }
-
-    const res = await fetch("/api/create/create-post", {
-        method: "POST",
-        headers: { "Content-Type": "Application/json" },
-        body: JSON.stringify(content)
     })
 
-    const data = await res.json();
+    thought.addEventListener("input", function () {
+        const thoughtValue = thought.value.trim();
+        if (thoughtValue) { postBtn.disabled = false }
+        else { postBtn.disabled = true };
+    })
 
-    createPostSpinner.classList.add("hidden");
+    postBtn.addEventListener("click", async function () {
+        const mediaVal = mediaPreview.getAttribute("src");
+        const thoughtVal = thought.value.trim();
 
-    if (!res.ok) {
-        createPostError.innerHTML = data.message;
-    }
+        createPostError.innerHTML = "";
 
-    const mediaFile = postMediaInput.files;
-    console.log(mediaVal);
+        createPostSpinner.classList.remove("hidden");
 
-    thought.value = "";
-    postMediaInput.value = "";
-    mediaPreview.classList.add("hidden");
-    postBtn.disabled = true;
 
-    window.location.reload();
-})
+        const content = {
+            url: mediaVal,
+            fileId: fileId,
+            thought: thoughtVal
+        }
+
+        const res = await fetch("/api/create/create-post", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(content)
+        })
+
+        const data = await res.json();
+
+        createPostSpinner.classList.add("hidden");
+
+        if (!res.ok) {
+            createPostError.innerHTML = data.message;
+            return;
+        }
+
+        const mediaFile = postMediaInput.files;
+        console.log(mediaVal);
+
+        thought.value = "";
+        postMediaInput.value = "";
+        mediaPreview.classList.add("hidden");
+        postBtn.disabled = true;
+
+        window.location.reload();
+    })
+}
+
+
+if (createPostModal) {
+    createPostFunc(createPostModal);
+}
+
+
+
+const newPostBtn = document.querySelector("#new-post-btn");
+if (newPostBtn) {
+    newPostBtn.addEventListener("click", function () {
+        swal.fire({
+            html: createPostModal.innerHTML,
+            showConfirmButton: false,
+            showCloseButton: true,
+            didOpen: () => {
+                const popup = Swal.getPopup();
+                createPostFunc(popup);
+            }
+        })
+    })
+}
 
 
 
