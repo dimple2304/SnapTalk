3fba2aeb6d562a2382e457e033c8092832199b87
dimple2304 <dimplekumar2304@gmail.com>
2026-06-13 12:51:46 +0530

    try send mail

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,38 +1,33 @@
-import nodemailer from 'nodemailer';
-import { EMAIL_ID, EMAIL_PASS } from "../config/envIndex.js";
+import { Resend } from "resend";
+import { RESEND_API_KEY } from "../config/envIndex.js";
+
+const resend = new Resend(RESEND_API_KEY);
 
 export const sendMail = async ({ email, subject, html }) => {
     if (!email) throw new error("Missing email");
     if (!subject) throw new error("Missing subject");
     if (!html) throw new error("Missing email body");
-
     try {
-
-        console.log("EMAIL_ID:", EMAIL_ID);
-        console.log("EMAIL_PASS exists:", !!EMAIL_PASS);
-        const transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false,
-            auth: {
-                user: EMAIL_ID,
-                pass: EMAIL_PASS,
-            },
-        });
-
-        const info = await transporter.sendMail({
-            from: `"SnapTalk" <${EMAIL_ID}>`,
+        const response = await resend.emails.send({
+            from: "SnapTalk <onboarding@resend.dev>",
             to: email,
             subject,
             html
         });
-        console.log(info);
+
+        console.log(response);
+
         return {
             success: true,
-            message: "OTP sent to your registered email id."
+            message: "OTP sent successfully"
         };
+
     } catch (err) {
-        console.log("Error occured", err);
-        return { success: false, message: "Failed to send OTP" };
+        console.error(err);
+
+        return {
+            success: false,
+            message: "Failed to send OTP"
+        };
     }
-}
+};
